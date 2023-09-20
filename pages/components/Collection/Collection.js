5a27c2f7157a59c7d78b8a9451dcb4296622ec08
evgenytryzo <g0022@inbox.ru>
2023-09-21 00:43:02 +0300

    finish component Collection

--- pages/components/Collection/Collection.js
+++ pages/components/Collection/Collection.js
@@ -2,30 +2,40 @@ import styles from "@/pages/components/Collection/Collection.module.scss"
 import React, { useState } from "react"
 import Image from "next/image"
 import wineImage from "../../../public/wineImage.png"
+import circlesImage from "../../../public/circle.svg"
+import circles_active from "../../../public/circle_active.svg"
+import prevItemImage from "../../../public/prevItem.png"
+import nextItemImage from "../../../public/nextItem.png"
 
 const Collection = ({ array }) => {
   const [ currentIndex, setCurrentIndex ] = useState(0)
 
   const nextItem = () => {
-    // Увеличиваем индекс на 1, оборачивая его к началу, если он выходит за границы массива
     setCurrentIndex((prevIndex) => (prevIndex + 1) % array.length)
   }
 
   const prevItem = () => {
-    // Уменьшаем индекс на 1, оборачивая его к концу, если он становится меньше 0
     setCurrentIndex((prevIndex) => (prevIndex - 1 + array.length) % array.length)
   }
+  const circles = Array.from({ length: 5 }, (_, index) => (
+      <div key={ index } className={ styles.containerCircles }>
+        { currentIndex % 5 === index % 5 ?
+          <Image className={ styles.circlesItem } src={ circles_active } alt="circles active"/> :
+          <Image className={ styles.circlesItem } src={ circlesImage } alt="circles"/> }
+      </div>
+    )
+  )
 
   const currentItem = array[ currentIndex ]
 
   return (
     <>
       <div className={ styles.container }>
-        <button onClick={ prevItem }>(-</button>
+        <Image className={ styles.button } src={ prevItemImage } onClick={ prevItem } alt="button"/>
         <div className={ styles.container__galary }>
           <Image className={ styles.wineImage } src={ wineImage } alt={ currentItem.name }/>
           <div className={ styles.info }>
-            <p className={ styles.info__about }>{ currentItem.name }</p>
+            <p className={ styles.info__about_bold }>{ currentItem.name }</p>
             <p className={ styles.info__about }>Аромат: { currentItem.smell.join(", ") }</p>
             <p className={ styles.info__about }>Вино { currentItem.species }, { currentItem.volume }</p>
             <p className={ styles.info__about }>Алкоголь: { currentItem.alcohol }%</p>
@@ -33,10 +43,10 @@ const Collection = ({ array }) => {
             <p className={ styles.info__about }>Рейтинг: { currentItem.rating }</p>
           </div>
         </div>
-        <button onClick={ nextItem }>-)</button>
+        <Image src={ nextItemImage } className={ styles.button } onClick={ nextItem } alt="button"/>
       </div>
-      <div className={ styles.swith }>
-        ВИДЖЕТ
+      <div className={ styles.switch }>
+        { circles }
       </div>
     </>
   )
--- pages/utils/varibles.js
+++ pages/utils/varibles.js
@@ -21,234 +21,11 @@ const linkName = [
   }
 ]
 
-const wine = [
-  {
-    name: "Pfefferer Sun",
-    type: "white",
-    smell: [ "blackberry" ],
-    classification: "Белое",
-    alcohol: 2.5,
-    sugar: 16,
-    rating: 4,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Merlot Reserve",
-    type: "red",
-    smell: [ "cherry", "plum" ],
-    classification: "Красное",
-    alcohol: 14.0,
-    sugar: 5,
-    rating: 4.3,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Chardonnay Dream",
-    type: "white",
-    smell: [ "apple", "vanilla" ],
-    classification: "Белое",
-    alcohol: 13.5,
-    sugar: 6,
-    rating: 4.2,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Rosé Blush",
-    type: "rose",
-    smell: [ "strawberry", "citrus" ],
-    classification: "Розовое",
-    alcohol: 12.8,
-    sugar: 7,
-    rating: 4.1,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Sapphire Blue",
-    type: "blue",
-    smell: [ "blueberry", "lavender" ],
-    classification: "Голубое",
-    alcohol: 11.2,
-    sugar: 8,
-    rating: 4.0,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Cabernet Sauvignon Classic",
-    type: "red",
-    smell: [ "black currant", "tobacco" ],
-    classification: "Красное",
-    alcohol: 14.5,
-    sugar: 4,
-    rating: 4.4,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Sauvignon Blanc Fresh",
-    type: "white",
-    smell: [ "green apple", "herbs" ],
-    classification: "Белое",
-    alcohol: 13.2,
-    sugar: 5,
-    rating: 4.3,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Pink Paradise",
-    type: "rose",
-    smell: [ "raspberry", "floral notes" ],
-    classification: "Розовое",
-    alcohol: 12.0,
-    sugar: 6,
-    rating: 4.5,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Blueberry Bliss",
-    type: "blue",
-    smell: [ "blue raspberry", "cherry pit" ],
-    classification: "Голубое",
-    alcohol: 11.5,
-    sugar: 7,
-    rating: 4.2,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Pinot Noir Elegance",
-    type: "red",
-    smell: [ "red cherry", "rose" ],
-    classification: "Красное",
-    alcohol: 14.2,
-    sugar: 5,
-    rating: 4.6,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Riesling Harmony",
-    type: "white",
-    smell: [ "peach", "honey" ],
-    classification: "Белое",
-    alcohol: 12.8,
-    sugar: 8,
-    rating: 4.0,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Sunset Rosé",
-    type: "rose",
-    smell: [ "orange", "almond" ],
-    classification: "Розовое",
-    alcohol: 12.3,
-    sugar: 4,
-    rating: 4.3,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Electric Blue",
-    type: "blue",
-    smell: [ "eucalyptus", "lime" ],
-    classification: "Голубое",
-    alcohol: 10.5,
-    sugar: 6,
-    rating: 4.1,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Malbec Velvet",
-    type: "red",
-    smell: [ "black cherry", "chocolate" ],
-    classification: "Красное",
-    alcohol: 13.9,
-    sugar: 5,
-    rating: 4.4,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Sémillon Sunshine",
-    type: "white",
-    smell: [ "pear", "lemon" ],
-    classification: "Белое",
-    alcohol: 11.6,
-    sugar: 7,
-    rating: 4.2,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Blush Blossom",
-    type: "rose",
-    smell: [ "pomegranate", "rose petals" ],
-    classification: "Розовое",
-    alcohol: 12.1,
-    sugar: 5,
-    rating: 4.7,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Azure Blue",
-    type: "blue",
-    smell: [ "blue raspberry", "vanilla" ],
-    classification: "Голубое",
-    alcohol: 11.8,
-    sugar: 8,
-    rating: 4.2,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Zinfandel Delight",
-    type: "red",
-    smell: [ "raspberry jam", "pepper" ],
-    classification: "Красное",
-    alcohol: 14.1,
-    sugar: 6,
-    rating: 4.5,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Sangiovese Symphony",
-    type: "red",
-    smell: [ "cherry", "earthiness" ],
-    classification: "Красное",
-    alcohol: 13.7,
-    sugar: 4,
-    rating: 4.3,
-    iconDesktop: null,
-    iconMobile: null
-  },
-  {
-    name: "Viognier Dream",
-    type: "white",
-    smell: [ "apricot", "honeysuckle" ],
-    classification: "Белое",
-    alcohol: 12.3,
-    sugar: 5,
-    rating: 4.6,
-    iconDesktop: null,
-    iconMobile: null
-  }
-]
-
 const wines = [
   {
     name: "Pfefferer Sun",
     type: "white",
-    smell: ["blackberry", "blackberry"],
+    smell: [ "blackberry", "blackberry" ],
     classification: "Белое",
     alcohol: 2.5,
     sugar: 16,
@@ -261,7 +38,7 @@ const wines = [
   {
     name: "Merlot Reserve",
     type: "red",
-    smell: ["cherry", "plum"],
+    smell: [ "cherry", "plum" ],
     classification: "Красное",
     alcohol: 14.0,
     sugar: 5,
@@ -274,7 +51,7 @@ const wines = [
   {
     name: "Chardonnay Dream",
     type: "white",
-    smell: ["apple", "vanilla"],
+    smell: [ "apple", "vanilla" ],
     classification: "Белое",
     alcohol: 13.5,
     sugar: 6,
@@ -284,7 +61,175 @@ const wines = [
     species: "сухое",
     volume: "0,75 л."
   },
-];
+  {
+    name: "Rosé Blush",
+    type: "rose",
+    smell: [ "strawberry", "citrus" ],
+    classification: "Розовое",
+    alcohol: 12.8,
+    sugar: 7,
+    rating: 4.1,
+    iconDesktop: null,
+    iconMobile: null,
+    species: "полу сладкое",
+    volume: "0,75 л."
+  },
+  {
+    name: "Sapphire Blue",
+    type: "blue",
+    smell: [ "blueberry", "lavender" ],
+    classification: "Голубое",
+    alcohol: 11.2,
+    sugar: 8,
+    rating: 4.0,
+    iconDesktop: null,
+    iconMobile: null,
+    species: "полу сладкое",
+    volume: "0,75 л."
+  },
+  {
+    name: "Cabernet Sauvignon Classic",
+    type: "red",
+    smell: [ "black currant", "tobacco" ],
+    classification: "Красное",
+    alcohol: 14.5,
+    sugar: 4,
+    rating: 4.4,
+    iconDesktop: null,
+    iconMobile: null,
+    species: "сухое",
+    volume: "0,75 л."
+  },
+  {
+    name: "Sauvignon Blanc Fresh",
+    type: "white",
+    smell: [ "green apple", "herbs" ],
+    classification: "Белое",
+    alcohol: 13.2,
+    sugar: 5,
+    rating: 4.3,
+    iconDesktop: null,
+    iconMobile: null,
+    species: "полу сладкое",
+    volume: "0,75 л."
+  },
+  {
+    name: "Pink Paradise",
+    type: "rose",
+    smell: [ "raspberry", "floral notes" ],
+    classification: "Розовое",
+    alcohol: 12.0,
+    sugar: 6,
+    rating: 4.5,
+    iconDesktop: null,
+    iconMobile: null,
+    species: "полу сладкое",
+    volume: "0,75 л."
+  },
+  {
+    name: "Blueberry Bliss",
+    type: "blue",
+    smell: [ "blue raspberry", "cherry pit" ],
+    classification: "Голубое",
+    alcohol: 11.5,
+    sugar: 7,
+    rating: 4.2,
+    iconDesktop: null,
+    iconMobile: null,
+    species: "полу сладкое",
+    volume: "0,75 л."
+  },
+  {
+    name: "Pinot Noir Elegance",
+    type: "red",
+    smell: [ "red cherry", "rose" ],
+    classification: "Красное",
+    alcohol: 14.2,
+    sugar: 5,
+    rating: 4.6,
+    iconDesktop: null,
+    iconMobile: null,
+    species: "сухое",
+    volume: "0,75 л."
+  },
+  {
+    name: "Riesling Harmony",
+    type: "white",
+    smell: [ "peach", "honey" ],
+    classification: "Белое",
+    alcohol: 12.8,
+    sugar: 8,
+    rating: 4.0,
+    iconDesktop: null,
+    iconMobile: null,
+    species: "сладкое",
+    volume: "0,75 л."
+  },
+  {
+    name: "Sunset Rosé",
+    type: "rose",
+    smell: [ "orange", "almond" ],
+    classification: "Розовое",
+    alcohol: 12.3,
+    sugar: 4,
+    rating: 4.3,
+    iconDesktop: null,
+    iconMobile: null,
+    species: "сладкое",
+    volume: "0,75 л."
+  },
+  {
+    name: "Electric Blue",
+    type: "blue",
+    smell: [ "eucalyptus", "lime" ],
+    classification: "Голубое",
+    alcohol: 10.5,
+    sugar: 6,
+    rating: 4.1,
+    iconDesktop: null,
+    iconMobile: null,
+    species: "полу сладкое",
+    volume: "0,75 л."
+  },
+  {
+    name: "Malbec Velvet",
+    type: "red",
+    smell: [ "black cherry", "chocolate" ],
+    classification: "Красное",
+    alcohol: 13.9,
+    sugar: 5,
+    rating: 4.4,
+    iconDesktop: null,
+    iconMobile: null,
+    species: "сухое",
+    volume: "0,75 л."
+  },
+  {
+    name: "Sémillon Sunshine",
+    type: "white",
+    smell: [ "pear", "lemon" ],
+    classification: "Белое",
+    alcohol: 11.6,
+    sugar: 7,
+    rating: 4.2,
+    iconDesktop: null,
+    iconMobile: null,
+    species: "полу сладкое",
+    volume: "0,75 л."
+  },
+  {
+    name: "Blush Blossom",
+    type: "rose",
+    smell: [ "pomegranate", "rose petals" ],
+    classification: "Розовое",
+    alcohol: 12.1,
+    sugar: 5,
+    rating: 4.7,
+    iconDesktop: null,
+    iconMobile: null,
+    species: "полу сладкое",
+    volume: "0,75 л."
+  } ]
 
 const wineType = [ "Красное", "Розовое", "Белое", "Голубое" ]
 export { wines, linkName, wineType }
